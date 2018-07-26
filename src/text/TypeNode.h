a4beeabf4f5fce3ee3826a756f58feaf950b2b0e
Tobe O <thosakwe@gmail.com>
2018-07-26 01:14:16 -0400

    TypeReferenceNode

---
+++ src/text/TypeNode.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2018, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file.
+#ifndef MANDA_TYPENODE_H
+#define MANDA_TYPENODE_H
+
+#include "AstNode.h"
+
+namespace manda
+{
+    class TypeNode : public AstNode
+    {
+
+    };
+}
+
+#endif //MANDA_TYPENODE_H
