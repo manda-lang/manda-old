9ab31bc87d82f0ffa9c1680e3f2203c7aa55dffd
Tobe O <thosakwe@gmail.com>
2018-09-14 12:33:47 -0400

    Basic entry point for ad hoc testing

---
+++ compiler/src/analysis/analysis.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2018, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file.
+#ifndef PROJECT_ANALYSIS_H
+#define PROJECT_ANALYSIS_H
+
+#include "MandaAnalyzer.h"
+#include "MandaCoreTypes.h"
+#include "MandaError.h"
+#include "MandaFunction.h"
+#include "MandaFunctionParameter.h"
+#include "MandaIntegerType.h"
+#include "MandaObject.h"
+#include "MandaObjectOrType.h"
+#include "MandaType.h"
+#include "SourceSpan.h"
+#include "SymbolTable.h"
+
+#endif //PROJECT_ANALYSIS_H
--- compiler/src/entrypoint/main.cpp
+++ compiler/src/entrypoint/main.cpp
@@ -4,6 +4,40 @@
 //
 // Use of this source code is governed by an
 // MIT-style license that can be found in the LICENSE file.
+#include <antlr4-runtime/antlr4-runtime.h>
+#include <iostream>
+#include "../analysis/analysis.h"
+
 int main() {
+    manda::MandaAnalyzer analyzer;
+    antlr4::ANTLRInputStream inputStream("x");
+    parser::MandaLexer lexer(&inputStream);
+    antlr4::CommonTokenStream tokens(&lexer);
+    parser::MandaParser parser(&tokens);
+
+    antlrcpp::Any valueAny = parser.expr()->accept(&analyzer);
+    auto &errors = analyzer.GetErrors();
+
+    if (errors.empty()) {
+        for (auto *error : errors) {
+            std::cerr << error->ToString() << std::endl;
+        }
+
+        return 1;
+    }
+    else {
+        if (valueAny.isNull()) {
+            std::cout << "Fail" << std::endl;
+        } else {
+            auto *value = valueAny.as<manda::MandaObjectOrType *>();
+
+            if (value->IsType()) {
+                std::cout << "Found type" << std::endl;
+            } else {
+                std::cout << "Found object" << std::endl;
+            }
+        }
+    }
+
     return 0;
 }
