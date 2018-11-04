0348009b0f7fe65c17bcee1880df11b271da6c65
Tobe O <thosakwe@gmail.com>
2018-11-04 00:13:39 -0400

    Remove pointers in binary expr

--- compiler/src/analysis/MandaAnalyzer.cpp
+++ compiler/src/analysis/MandaAnalyzer.cpp
@@ -32,7 +32,7 @@ void manda::MandaAnalyzer::visitExprStmt(MandaParser::ExprStmtContext *ctx) {
                 MandaError::kError,
                 "Evaluating this expression produced an error.",
                 SourceSpan(*ctx));
-        return Any();
+        return;
     } else {
         auto *value = valueAny.as<MandaObjectOrType *>();
         // TODO: Only allow calls here
@@ -49,7 +49,7 @@ void manda::MandaAnalyzer::visitReturnStmt(MandaParser::ReturnStmtContext *ctx) 
                 MandaError::kError,
                 "Evaluating this expression produced an error.",
                 SourceSpan(*ctx));
-        return Any();
+        return;
     } else {
         auto *value = valueAny.as<MandaObjectOrType *>();
         // TODO: Only allow calls here
@@ -58,115 +58,103 @@ void manda::MandaAnalyzer::visitReturnStmt(MandaParser::ReturnStmtContext *ctx) 
     }
 }
 
-manda::MandaObjectOrType & manda::MandaAnalyzer::resolveBinary(antlr4::ParserRuleContext *ctx,
-                                                               MandaParser::ExprContext *leftCtx,
-                                                               MandaParser::ExprContext *rightCtx,
-                                                               const std::string &op) {
-    Any leftAny = leftCtx->accept(this), rightAny = rightCtx->accept(this);
+manda::MandaObjectOrType &manda::MandaAnalyzer::resolveBinary(antlr4::ParserRuleContext *ctx,
+                                                              MandaParser::ExprContext *leftCtx,
+                                                              MandaParser::ExprContext *rightCtx,
+                                                              const std::string &op) {
+    auto leftAny = visitExpr(leftCtx), rightAny = visitExpr(rightCtx);
 
-    if (leftAny.isNull() || rightAny.isNull()) {
-        errors.emplace_back(MandaError::kError, "Could not resolve this binary expression.",
-                            SourceSpan(*ctx));
+    if (dynamic_cast<MandaType *>(&leftAny) || dynamic_cast<MandaType *>(&rightAny)) {
+        errors.emplace_back(
+                MandaError::kError,
+                "Binary operators may not be applied directly to types.",
+                SourceSpan(*ctx));
         return Any();
     } else {
+        auto &left = static_cast<MandaObject &>(leftAny), &right = static_cast<MandaObject &>(rightAny);
 
-        if (leftAny.is<MandaType *>() || rightAny.is<MandaType *>()) {
+        // Binary operators must be applied to values of compatible types.
+        if (!(left.type.isAssignableTo(right.type))) {
             errors.emplace_back(
                     MandaError::kError,
-                    "Binary operators may not be applied directly to types.",
+                    "Cannot perform a binary operation on two values of incompatible types.",
                     SourceSpan(*ctx));
             return Any();
-        } else {
-            auto *left = leftAny.as<MandaObject *>(), *right = rightAny.as<MandaObject *>();
-
-            // Binary operators must be applied to values of compatible types.
-            if (!(left->type.isAssignableTo(right->type))) {
-                errors.emplace_back(
-                        MandaError::kError,
-                        "Cannot perform a binary operation on two values of incompatible types.",
-                        SourceSpan(*ctx));
-                return Any();
-            }
-
-            try {
-                // Let the left type handle the computation.
-                auto result = left->type.binaryOperation(*left, *right, op,
-                                                         SourceSpan(*ctx));
-                return Any(&result); // TODO: Untracked pointer?
-            } catch (const MandaException &exc) {
-                errors.push_back(exc.error);
-                return Any();
-            }
         }
+
+        // Let the left type handle the computation.
+        auto result = left.type.binaryOperation(left, right, op,
+                                                SourceSpan(*ctx));
+        return result; // TODO: Untracked pointer?
     }
 }
 
-manda::MandaObjectOrType & manda::MandaAnalyzer::visitMulDivOrModExpr(MandaParser::MulDivOrModExprContext *ctx) {
+manda::MandaObjectOrType &manda::MandaAnalyzer::visitMulDivOrModExpr(MandaParser::MulDivOrModExprContext *ctx) {
     return resolveBinary(ctx, ctx->left, ctx->right, ctx->op->getText());
 }
 
-manda::MandaObjectOrType & manda::MandaAnalyzer::visitAddOrSubExpr(MandaParser::AddOrSubExprContext *ctx) {
+manda::MandaObjectOrType &manda::MandaAnalyzer::visitAddOrSubExpr(MandaParser::AddOrSubExprContext *ctx) {
     return resolveBinary(ctx, ctx->left, ctx->right, ctx->op->getText());
 }
 
-manda::MandaObjectOrType & manda::MandaAnalyzer::visitBoolAndOrExpr(MandaParser::BoolAndOrExprContext *ctx) {
+manda::MandaObjectOrType &manda::MandaAnalyzer::visitBoolAndOrExpr(MandaParser::BoolAndOrExprContext *ctx) {
     return resolveBinary(ctx, ctx->left, ctx->right, ctx->op->getText());
 }
 
-manda::MandaObjectOrType & manda::MandaAnalyzer::visitBoolEqOrNeqExpr(MandaParser::BoolEqOrNeqExprContext *ctx) {
+manda::MandaObjectOrType &manda::MandaAnalyzer::visitBoolEqOrNeqExpr(MandaParser::BoolEqOrNeqExprContext *ctx) {
     return resolveBinary(ctx, ctx->left, ctx->right, ctx->op->getText());
 }
 
-manda::MandaObject & manda::MandaAnalyzer::visitTrueExpr(MandaParser::TrueExprContext *ctx) {
-    auto *object = new MandaObject(coreTypes.boolType, SourceSpan(*ctx));
-    object->constantValueType = MandaObject::kBool;
-    object->constantValue.asBool = true;
-    return Any(object);
+manda::MandaObject &manda::MandaAnalyzer::visitTrueExpr(MandaParser::TrueExprContext *ctx) {
+    MandaObject object(coreTypes.boolType, SourceSpan(*ctx));
+    object.constantValueType = MandaObject::kBool;
+    object.constantValue.asBool = true;
+    return object;
 }
 
-manda::MandaObject & manda::MandaAnalyzer::visitFalseExpr(MandaParser::FalseExprContext *ctx) {
-    auto *object = new MandaObject(coreTypes.boolType, SourceSpan(*ctx));
-    object->constantValueType = MandaObject::kBool;
-    object->constantValue.asBool = false;
-    return Any(object);
+manda::MandaObject &manda::MandaAnalyzer::visitFalseExpr(MandaParser::FalseExprContext *ctx) {
+    MandaObject object(coreTypes.boolType, SourceSpan(*ctx));
+    object.constantValueType = MandaObject::kBool;
+    object.constantValue.asBool = false;
+    return object;
 }
 
 
-manda::MandaObject & manda::MandaAnalyzer::visitIntegerExpr(MandaParser::IntegerExprContext *ctx) {
+manda::MandaObject &manda::MandaAnalyzer::visitIntegerExpr(MandaParser::IntegerExprContext *ctx) {
     uint64_t value = strtoul(ctx->getText().c_str(), nullptr, 10);
-    auto *object = new MandaObject(coreTypes.int32Type, SourceSpan(*ctx));
-    object->constantValueType = MandaObject::kSigned;
-    object->constantValue.asSigned = value;
-    return Any(object);
+    MandaObject object(coreTypes.int32Type, SourceSpan(*ctx));
+    object.constantValueType = MandaObject::kSigned;
+    object.constantValue.asSigned = value;
+    return object;
 }
 
-manda::MandaObject & manda::MandaAnalyzer::visitHexExpr(MandaParser::HexExprContext *ctx) {
+manda::MandaObject &manda::MandaAnalyzer::visitHexExpr(MandaParser::HexExprContext *ctx) {
     uint64_t value = stoul(ctx->getText(), nullptr, 16);
-    auto *object = new MandaObject(coreTypes.int32Type, SourceSpan(*ctx));
-    object->constantValueType = MandaObject::kSigned;
-    object->constantValue.asSigned = value;
-    return Any(object);
+    MandaObject object(coreTypes.int32Type, SourceSpan(*ctx));
+    object.constantValueType = MandaObject::kSigned;
+    object.constantValue.asSigned = value;
+    return object;
 }
 
-manda::MandaObject & manda::MandaAnalyzer::visitFloatExpr(MandaParser::FloatExprContext *ctx) {
+manda::MandaObject &manda::MandaAnalyzer::visitFloatExpr(MandaParser::FloatExprContext *ctx) {
     // TODO: Parse float -> Float32
     return MandaBaseVisitor::visitFloatExpr(ctx);
 }
 
-manda::MandaObjectOrType & manda::MandaAnalyzer::visitIdentifierExpr(MandaParser::IdentifierExprContext *ctx) {
+manda::MandaObjectOrType &manda::MandaAnalyzer::visitIdentifierExpr(MandaParser::IdentifierExprContext *ctx) {
     auto name = ctx->Identifier()->getText();
 
     try {
         auto &symbol = currentScope.resolve(name);
-        return Any(static_cast<MandaObjectOrType*>(&symbol.value)); // TODO: Unwatched pointer
+        return Any(static_cast<MandaObjectOrType *>(&symbol.value)); // TODO: Unwatched pointer
     } catch (const SymbolTableException &exc) {
         errors.emplace_back(MandaError::kError, exc.message, SourceSpan(*ctx));
         return Any();
     }
 }
 
-manda::MandaObjectOrType & manda::MandaAnalyzer::visitParenExpr(MandaParser::ParenExprContext *ctx) {
-    return ctx->expr()->accept(this);
+manda::MandaObjectOrType &manda::MandaAnalyzer::visitParenExpr(MandaParser::ParenExprContext *ctx) {
+    return visitExpr(ctx->expr());
 }
 
 MandaParser::CompilationUnitContext *manda::MandaAnalyzer::GetCompilationUnit() const {
@@ -174,7 +162,7 @@ MandaParser::CompilationUnitContext *manda::MandaAnalyzer::GetCompilationUnit() 
 }
 
 manda::MandaObjectOrType &manda::MandaAnalyzer::visitExpr(MandaParser::ExprContext *ctx) {
-    if (auto *trueCtx = dynamic_cast<MandaParser::TrueExprContext*>(ctx)) {
+    if (auto *trueCtx = dynamic_cast<MandaParser::TrueExprContext *>(ctx)) {
         return visitTrueExpr(trueCtx);
     }
 }
