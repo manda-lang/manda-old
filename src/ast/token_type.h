9cd9ecd3f57d9ae4e461ac25fe5e4373aea3e9fa
Tobe O <thosakwe@gmail.com>
2018-12-01 01:55:55 -0500

    Token

---
+++ src/ast/token_type.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2018, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file.
+#ifndef PROJECT_TOKEN_TYPE_H
+#define PROJECT_TOKEN_TYPE_H
+
+namespace manda
+{
+    enum TokenType
+    {
+        // Values
+        ID
+    };
+}
+
+#endif //PROJECT_TOKEN_TYPE_H
