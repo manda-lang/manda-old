d41da7415f3853a563a684878cba01c56d1f3b29
Tobe O <thosakwe@gmail.com>
2019-03-02 22:30:03 -0500

    Impl VM.spawnThread

--- manda/src/runtime/VirtualMachine.cpp
+++ manda/src/runtime/VirtualMachine.cpp
@@ -4,4 +4,13 @@
 //
 // Use of this source code is governed by a
 // license that can be found in the LICENSE file.
+#include <memory>
 #include <manda/runtime/runtime.hpp>
+
+std::unique_ptr<manda::runtime::Thread> &manda::runtime::VirtualMachine::spawnThread()
+{
+    auto thread = std::make_unique<manda::runtime::Thread>();
+    threads.push_back(thread);
+    return threads.at(threads.size() - 1);
+}
+
