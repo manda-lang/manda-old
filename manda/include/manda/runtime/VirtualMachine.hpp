c03b8176b9bc7e1aa8f1766a766e76ab993cb616
Tobe O <thosakwe@gmail.com>
2019-03-02 22:22:04 -0500

    Base VirtualMachine class

---
+++ manda/include/manda/runtime/VirtualMachine.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2019, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+#ifndef MANDA_VIRTUALMACHINE_HPP
+#define MANDA_VIRTUALMACHINE_HPP
+
+namespace manda {
+    namespace runtime {
+        class VirtualMachine {
+
+        };
+    }
+}
+
+#endif //MANDA_VIRTUALMACHINE_HPP
