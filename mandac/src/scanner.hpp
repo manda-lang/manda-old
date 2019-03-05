3f1bee1a615a19eecd65f97d6f3180ab26b415c7
Tobe O <thosakwe@gmail.com>
2019-03-05 11:09:33 -0500

    Scanner + Token init

---
+++ mandac/src/scanner.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2019, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+#ifndef MANDA_SCANNER_HPP
+#define MANDA_SCANNER_HPP
+
+
+class Scanner
+{
+
+};
+
+
+#endif //MANDA_SCANNER_HPP
---
+++ mandac/src/token.hpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2019, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+#ifndef MANDA_TOKEN_HPP
+#define MANDA_TOKEN_HPP
+
+#include <string>
+
+namespace mandac
+{
+    namespace TokenType
+    {
+        enum Enum
+        {
+
+        };
+    }
+
+    struct Token
+    {
+        TokenType::Enum type;
+        std::string text;
+        unsigned long line, column;
+    };
+}
+
+
+#endif //MANDA_TOKEN_HPP
